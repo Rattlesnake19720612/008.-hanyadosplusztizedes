26d7a7aef9d32c9146da74c6ce6703f293a95f32
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 21:20:29 +0100

    Egész számok és tizedes törtek osztására

---
+++ 008. hanyadosplusztizedes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008.hanyadosplusztizedes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            double szam1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            double szam2 = Convert.ToDouble(Console.ReadLine());
+
+            double osztas = szam1 / szam2;
+            Console.WriteLine("A két szám hányadosa: " + osztas + "." + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
